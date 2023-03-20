9feffe9a9ee242f768518f49e8f1e2d2a97db28e
willyal1234 <willyalprido02@gmail.com>
2023-03-20 16:24:35 +0700

    Wily_1765
    Projek Kampus

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,14 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Megawati
+ Saya tinggal di daerah Jakarta 
+ Hobi yang saya sukai adalah
+Menjual Pulau<br><br>";
+
+echo "<h1>Partai PDIP</h1>";
+ }
+}
